4188b9022e5d358b8817b77006e0afff4c20cbb8
Jen Pierce <jenpierce42@gmail.com>
2016-03-20 12:08:29 -0700

    Consolidated list of ingredients on recipe show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+
+  def consolidate_ingredients(recipes)
+    # Takes an array of recipes (can contain just one recipe)
+    # Consolidates multiple entries for ingredients with same name into one row
+    # TODO: Actually calculate total amount required; for now just concatenates
+    # the different measurements.
+
+    # XXX For now assume just one recipe
+    recipe = recipes[0]
+    consolidated_ingredients = {}
+    recipe.ingredients.each do |ing|
+      if !consolidated_ingredients.key?(ing.name)
+        # Add new entry
+        consolidated_ingredients[ing.name] = "#{ing.amount.to_s} #{ing.unit}"
+      else
+        # Add this amount to existing ingredient entry
+        consolidated_ingredients[ing.name] = "#{consolidated_ingredients[ing.name]} plus #{ing.amount.to_s} #{ing.unit}"
+      end
+    end
+
+    consolidated_ingredients
+  end
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,11 @@
 class Recipe < ActiveRecord::Base
   has_many :steps
+  has_many :ingredients, through: :steps
 
   accepts_nested_attributes_for :steps, reject_if: :reject_steps, allow_destroy: true
 
   def reject_steps(attributes)
     attributes['description'].blank?
   end
+
 end
